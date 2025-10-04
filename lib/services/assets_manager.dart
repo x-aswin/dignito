ac95ede49fd82b64d6c695c1cee50b7502af0dbb
Aswin S <aswin102004@gmail.com>
2025-10-04 21:27:51 +0530

    map assest based on id

--- lib/services/assets_manager.dart
+++ lib/services/assets_manager.dart
@@ -1,12 +1,12 @@
 // assets_manager.dart
 class FestAssets {
   static const Map<String, String> backgroundImages = {
-    'dignito': 'assets/images/splash_back.png',
-    'daksh': 'assets/images/daksh_background.png',
+    '1': 'assets/images/splash_back.png',
+    '5': 'assets/images/daksh_background.png',
   };
 
   static const Map<String, String> logos = {
-    'dignito': 'assets/images/dignito_logo.png',
-    'daksh': 'assets/images/daksshtext.png',
+    '1': 'assets/images/dignito_logo.png',
+    '5': 'assets/images/daksshtext.png',
   };
 }
