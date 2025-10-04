79ea028aea82c0007dc8fdd43e9f7bad89554f10
Aswin S <aswin102004@gmail.com>
2025-10-04 21:23:55 +0530

    new dart file that maps to assets

---
+++ lib/services/assets_manager.dart
@@ -0,0 +1,12 @@
+// assets_manager.dart
+class FestAssets {
+  static const Map<String, String> backgroundImages = {
+    'dignito': 'assets/images/splash_back.png',
+    'daksh': 'assets/images/daksh_background.png',
+  };
+
+  static const Map<String, String> logos = {
+    'dignito': 'assets/images/dignito_logo.png',
+    'daksh': 'assets/images/daksshtext.png',
+  };
+}
